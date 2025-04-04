75dd34acf01294956d88caad0bcd849bcf7d44bf
Petr <red19650226@yandex.ru>
2025-04-04 20:57:28 +0300

    Cleanup

--- src/ru/practicum/dinner/Menu.java
+++ src/ru/practicum/dinner/Menu.java
@@ -10,19 +10,6 @@ class Menu {
 
     Menu() {
         menu = new HashMap<>();
-
-        // Mock-data
-        menu.put("Напиток", new ArrayList<>(List.of("Сок фруктовый", "Кола", "Морс", "Компот ягодный",
-                "Квас", "Минеральная вода")));
-
-        menu.put("Суп", new ArrayList<>(List.of("Щи", "Борщ", "Харчо", "Рассольник", "Крем-суп",
-                "Суп гороховый")));
-
-        menu.put("Горячее", new ArrayList<>(List.of("Шашлык свиной", "Плов", "Котлеты по-Киевски",
-                "Бефстроганов", "Жаркое", "Гуляш")));
-
-        menu.put("Салат", new ArrayList<>(List.of("салат Цезарь", "салат Мимоза", "салат Овощной",
-                "Сельдь под шубой", "Капуста квашеная", "салат Греческий")));
     }
 
     boolean addNewDish(String dishType, String dishName) {
@@ -45,5 +32,4 @@ class Menu {
     Map<String, List<String>> getMenu() {
         return menu;
     }
-
 }
